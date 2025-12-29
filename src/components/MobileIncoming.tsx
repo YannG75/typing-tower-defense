288559b82d4f978b91df7cd00d5f12bd3596b1f4
YannG75 <shadowyann@hotmail.com>
2025-12-29 18:20:34 +0100

    feat: adjust mobile placeholder styles and message for better clarity

--- src/components/MobileIncoming.tsx
+++ src/components/MobileIncoming.tsx
@@ -1,10 +1,10 @@
 
 export const MobileIncoming = () => {
     return (
-        <div style={{display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems:'center', textAlign:'center', gap: 20, padding: '0 5px', fontSize: '2vw', fontWeight: 'bold'}}
+        <div style={{display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems:'center', textAlign:'center', gap: 20, padding: '0 5px', fontSize: '3vw', fontWeight: 'bold'}}
         >
             <h1 style={{color: '#ffd93d'}}>Mobile adaptation Incoming</h1>
-            <h2 style={{color: '#906bcf'}}>Sorry about that but i'm working on the mobile adaptation right now and it will be available soon</h2>
+            <h2 style={{color: '#906bcf'}}>Sorry about that but i'm working on the mobile adaptation right now and it will be available soon. Try it out on desktop for now 😁</h2>
         </div>
     )
 }
